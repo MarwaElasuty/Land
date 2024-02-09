1d32c13a363f731a214d898f8a91ff062b0ced8f
Marwa Mohammed <52058731+MarwaElasuty@users.noreply.github.com>
2024-02-09 14:00:32 +0200

    land

---
+++ index.js
@@ -0,0 +1,13 @@
+var toggle = document.querySelector(".ul-list");
+       function appear(){
+        if(toggle.style.display=='none'){
+          toggle.style.display='block';
+        }
+        else{
+          toggle.style.display='none';
+        }
+       }
+
+
+
+       
