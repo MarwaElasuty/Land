47ec743b86d93b834fbe1ee020355f4da46a6f03
Marwa Mohammed <52058731+MarwaElasuty@users.noreply.github.com>
2024-02-18 17:09:41 +0200

    dsae

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-var toggle = document.querySelector(".ul-list");
-       function appear(){
-        if(toggle.style.display=='none'){
-          toggle.style.display='block';
-        }
-        else{
-          toggle.style.display='none';
-        }
-       }
-
-
-
-       
+function appear() {
+  document.querySelector(".ul-list").classList.toggle("display");
+ }
